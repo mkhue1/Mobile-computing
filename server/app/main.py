81b60a5e37781732fe586ff938e4f9895b44fd11
HG917 <gaohenry.917@outlook.com>
2026-09-21 15:36:15 +1000

    Added friends & groups backend functionality

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 
 from app.routers.users import router as users_router
+from app.routers.friends import router as friends_router
+from app.routers.friend_groups import (
+    router as friend_groups_router,
+)
 
 
 app = FastAPI()
 
-app.include_router(users_router)
+
+app.include_router(
+    users_router
+)
+
+app.include_router(
+    friends_router
+)
+
+app.include_router(
+    friend_groups_router
+)
 
 
 @app.get("/health")
--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,1 +1,11 @@
 from app.models.user import User
+
+from app.models.friendship import (
+    Friendship,
+    FriendshipStatus,
+)
+
+from app.models.friend_group import (
+    FriendGroup,
+    FriendGroupMember,
+)
